133d0aa7129ebcc00ec848caebec705da919530f
mattmeyerink <meyerink@umich.edu>
2020-12-03 15:32:52 -0600

    Small change

--- app/blueprints/blog/routes.py
+++ app/blueprints/blog/routes.py
@@ -1,12 +1,15 @@
 from flask import jsonify, request
 from .models import BlogPost
 from . import blog_bp
+from app.blueprints.authentication.models import User
+
 
 
 @blog_bp.route("/posts", methods=['GET'])
 def show_all_blog_posts():
     """Output all of the blog posts."""
-    return jsonify([post.to_dict() for post in BlogPost.query.all()])
+    posts = [post.to_dict() for post in BlogPost.query.all()]
+    return jsonify(posts)
 
 @blog_bp.route("/posts/<int:id>", methods=['GET'])
 def show_one_blog_post(id):
